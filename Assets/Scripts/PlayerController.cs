79df2d16d5058010ed5f244d5fe7f6e5b04a178c
Megalon <megalongaming@gmail.com>
2023-04-29 11:25:10 -0700

    Tighten movement controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,14 +7,14 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float _moveSpeed = 4f;
-    [SerializeField]
-    private float _jumpForce = 10f;
 
     [SerializeField]
     private BoxCollider _movementBounds;
+    [SerializeField]
+    private float _jumpForce;
 
-    private InputAction _moveAction;
     private Rigidbody _rb;
+    private InputAction _moveAction;
     private string _actionMapName = "gameplay";
     private Vector2 _moveVector;
     private bool _isXOutOfBounds;
@@ -40,28 +40,10 @@ public class PlayerController : MonoBehaviour
 
     public void Move(Vector2 moveVector)
     {
+        transform.Translate(moveVector.x * _moveSpeed * Time.deltaTime, 0, 0, Space.Self);
 
-        if (_isXOutOfBounds)
-        {
-            // Get the closest point we're out of bounds to
-            Vector3 closestPoint = _movementBounds.ClosestPoint(transform.position);
-
-            // Move back towards the point on the bounds
-            moveVector.x = Mathf.Sign(closestPoint.x - transform.position.x);
-        }
-
-        _rb.AddForce(new Vector3(moveVector.x, 0, 0) * _moveSpeed * Time.deltaTime, ForceMode.Impulse);
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject != _movementBounds.gameObject) return;
-
-        _isXOutOfBounds = true;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        _isXOutOfBounds = false;
+        // Keep object within x bounds
+        float clampedX = Mathf.Clamp(transform.position.x, -_movementBounds.bounds.extents.x, _movementBounds.bounds.extents.x);
+        transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
     }
 }
